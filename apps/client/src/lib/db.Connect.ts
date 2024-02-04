cb370fc7ccc45a1c2129632565a4ef37bbe6a12d
Kushagra <skushagra.sharma@gmail.com>
2024-02-05 01:45:29 +0530

    fix2

--- apps/client/src/lib/db.Connect.ts
+++ apps/client/src/lib/db.Connect.ts
@@ -1,39 +1,39 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-let alreadyConnected = false;
+// let alreadyConnected = false;
 
-export const connectToDatabase = async () => {
-  try {
-    if (alreadyConnected) {
-      console.log("Already connected to the database");
-      return;
-    }
+// export const connectToDatabase = async () => {
+//   try {
+//     if (alreadyConnected) {
+//       console.log("Already connected to the database");
+//       return;
+//     }
 
-    console.log("here")
-    // Connect to MongoDB
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+//     console.log("here")
+//     // Connect to MongoDB
+//     await mongoose.connect(process.env.MONGO_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
 
-    // Connection successful
-    console.log("Connected to the database");
+//     // Connection successful
+//     console.log("Connected to the database");
 
-    // Optional: Add event listeners for connection events
-    mongoose.connection.on('error', (err) => {
-      console.error(`MongoDB connection error: ${err}`);
-    });
+//     // Optional: Add event listeners for connection events
+//     mongoose.connection.on('error', (err) => {
+//       console.error(`MongoDB connection error: ${err}`);
+//     });
 
-    mongoose.connection.on('disconnected', () => {
-      console.log("MongoDB disconnected");
-    });
+//     mongoose.connection.on('disconnected', () => {
+//       console.log("MongoDB disconnected");
+//     });
 
-    // Set the flag to indicate that the connection has been established
-    alreadyConnected = true;
-  } catch (error) {
-    console.error(`Error connecting to the database`);
-    // Handle the error appropriately
-  }
-};
+//     // Set the flag to indicate that the connection has been established
+//     alreadyConnected = true;
+//   } catch (error) {
+//     console.error(`Error connecting to the database`);
+//     // Handle the error appropriately
+//   }
+// };
 
 
